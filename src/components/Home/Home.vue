92f2af884105508f30274102ab5ce7dee8768876
hugosgomes <hdsg0807@gmail.com>
2021-02-17 09:35:35 -0300

    Css separado em arquivos específicos.

--- src/components/Home/Home.vue
+++ src/components/Home/Home.vue
@@ -1,15 +1,17 @@
 <template>
   <div class="container">
-    <h1>Pokedex</h1>
-    <div class="pages">
-      <button @click="previusPage" :disabled="page === 1">
-        <i class="fas fa-step-backward"></i>
-      </button>
-      <h3>Page {{ page }}</h3>
-      <button @click="nextPage" :disabled="page === 38">
-        <i class="fas fa-step-forward"></i>
-      </button>
-    </div>
+    <header>
+      <h1>Pokedex</h1>
+      <div class="pages">
+        <button @click="previusPage" :disabled="page === 1">
+          <i class="fas fa-step-backward"></i>
+        </button>
+        <h3>Page {{ page }}</h3>
+        <button @click="nextPage" :disabled="page === 38">
+          <i class="fas fa-step-forward"></i>
+        </button>
+      </div>
+    </header>
     <section>
       <Card
         v-for="pokemon in pokemons"
@@ -74,54 +76,5 @@ export default {
 </script>
 
 <style scoped>
-.container {
-  background: linear-gradient(to right, rgb(197, 230, 236), rgb(239, 187, 230));
-  padding: 20px;
-  height: 100%;
-}
-
-.container section {
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
-  gap: 17px;
-  min-height: 100vh;
-}
-
-.pages,
-.pages-bottom {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  column-gap: 20px;
-}
-
-.pages {
-  margin-bottom: 20px;
-}
-
-.pages-bottom {
-  margin-top: 20px;
-}
-
-.container h1 {
-  text-align: center;
-  margin-bottom: 20px;
-}
-
-button {
-  background: transparent;
-  border: none;
-  font-size: 30px;
-}
-
-button:hover {
-  cursor: pointer;
-  color: orangered;
-}
-
-button:disabled:hover {
-  cursor: auto;
-  color: #1010104d;
-}
+@import "./Home.css";
 </style>
